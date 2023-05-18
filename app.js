e07a96e1a4c843084432c0bc541140c4b9547741
Daniil <borovovdaniil@gmail.com>
2023-05-19 04:06:07 +0600

    add 404 handler

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use((req, res, next) => {
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 // Start the server
 app.listen(PORT, () => {
