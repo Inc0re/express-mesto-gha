6a559516185dce907c81b48733ea4020115b44ee
Daniil <borovovdaniil@gmail.com>
2023-05-19 22:26:23 +0600

    add dislikeCard

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,18 +1,29 @@
 const Card = require('../models/card');
 
+// Errors: 500 - server error
 const getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+// Errors: 400 - bad request, 500 - server error
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании карточки',
+        });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
+// Errors: 404 - not found, 500 - server error
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
@@ -27,11 +38,12 @@ const deleteCard = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+// Errors: 400 - bad request, 404 - not found, 500 - server error
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
-    { new: true, runValidators: true }
+    { new: true, runValidators: true },
   )
     .then((card) => {
       if (!card) {
@@ -46,7 +58,38 @@ const likeCard = (req, res) => {
       if (err.name === 'CastError') {
         res
           .status(400)
-          .send({ message: 'Переданы некорректные данные для постановки лайка' });
+          .send({
+            message: 'Переданы некорректные данные для постановки лайка',
+          });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+// Errors: 400 - bad request, 404 - not found, 500 - server error
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true, runValidators: true },
+  )
+    .then((card) => {
+      if (!card) {
+        res
+          .status(404)
+          .send({ message: `Нет карточки с id ${req.params.cardId}` });
+      } else {
+        res.send({ data: card });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res
+          .status(400)
+          .send({
+            message: 'Переданы некорректные данные для снятия лайка',
+          });
       } else {
         res.status(500).send({ message: err.message });
       }
@@ -58,4 +101,5 @@ module.exports = {
   createCard,
   deleteCard,
   likeCard,
+  dislikeCard,
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 
-const { createCard, getCards, deleteCard, likeCard } = require('../controllers/cards');
+const {
+  createCard,
+  getCards,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
 
 router.post('/', createCard);
 router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
+router.delete('/:cardId/likes', dislikeCard);
 router.put('/:cardId/likes', likeCard);
 
 module.exports = router;
