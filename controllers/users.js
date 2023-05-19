65d83af7eecfc3822cd9104f7051aa87f9702165
Daniil <borovovdaniil@gmail.com>
2023-05-19 18:41:15 +0600

    add updateUser and error handlers

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 const User = require('../models/user');
 
+// Errors: 500 - server error
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+// Errors: 400 - bad request, 404 - not found, 500 - server error
 const getUserById = (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
@@ -17,14 +19,57 @@ const getUserById = (req, res) => {
       }
       res.send({ data: user });
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: `Некорректный id пользователя ${req.params.id}` });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
+// Errors: 400 - bad request, 500 - server error
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
-module.exports = { getUsers, getUserById, createUser };
+//
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении профиля',
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
-const { createUser, getUsers, getUserById } = require('../controllers/users');
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+} = require('../controllers/users');
 
 router.post('/', createUser);
 router.get('/', getUsers);
 router.get('/:id', getUserById);
+router.patch('/me', updateUser);
 
 module.exports = router;
